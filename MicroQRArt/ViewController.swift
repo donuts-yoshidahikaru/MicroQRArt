ab84d0dfbbf6545a810f714f4876adbce576544e
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-28 10:24:02 +0900

    initial configuration

---
+++ MicroQRArt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MicroQRArtTests/MicroQRArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  MicroQRArtTests.swift
+//  MicroQRArtTests
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+import Testing
+@testable import MicroQRArt
+
+struct MicroQRArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
