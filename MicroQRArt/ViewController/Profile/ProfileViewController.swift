22c51bd663086f783f24bd753001de946660767d
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-28 14:46:32 +0900

    feat(vc): ユーザ画面の初期コンポーネント追加

---
+++ MicroQRArt/ViewController/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
