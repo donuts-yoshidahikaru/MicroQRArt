ebdeb9e95bac7719e6344facd5a5ed3a73193db0
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-28 16:19:56 +0900

    chore(color): 基本の色構成を追加
    - background: 背景色
    - primaryContent: 主要コンテンツ色
    - content: 標準コンテンツ色
    - secondaryContent: 副次コンテンツ色
    - subtleContent: 補助コンテンツ色

---
+++ MicroQRArt/Extension/UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background = #colorLiteral(red: 0.09019607843, green: 0.1294117647, blue: 0.07058823529, alpha: 1)
+    static let primaryContent = #colorLiteral(red: 0.3294117647, green: 0.8196078431, blue: 0.168627451, alpha: 1)
+    static let content = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let secondaryContent = #colorLiteral(red: 0.63137254899999995, green: 0.76078431369999999, blue: 0.59999999999999998, alpha: 1)
+    static let subtleContent = #colorLiteral(red: 0.18039215689999999, green: 0.25882352939999997, blue: 0.16078431369999999, alpha: 1)
+}
