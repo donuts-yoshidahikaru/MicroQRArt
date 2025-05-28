1078986481652530d5b765496c1684ebbb38e887
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-28 14:44:37 +0900

    feat(vc): 装飾画面の初期コンポーネント追加

---
+++ MicroQRArt/ViewController/Decoration/DecorationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DecorationViewController.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+import UIKit
+
+final class DecorationViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
