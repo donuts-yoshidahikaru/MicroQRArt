949aabfb8eb58b413e4f41234d83463672c9814d
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-28 18:25:44 +0900

    feat(type): VCのtypeを追加

---
+++ MicroQRArt/Value/ParentViewControllerType.swift
@@ -0,0 +1,11 @@
+/// NavigationViewのボタン種別を表す。
+///
+/// home: ホームボタン
+/// profile: プロフィールボタン
+/// decoration: デコレーションボタン
+///
+enum ParentViewControllerType {
+    case home
+    case profile
+    case decoration
+} 
