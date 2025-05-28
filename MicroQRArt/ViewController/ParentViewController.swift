2d046eba63a601d489602f0ac07da162486e35e7
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-28 16:39:44 +0900

    feat(vc): navViewを汎用的に使用するvcを追加

---
+++ MicroQRArt/ViewController/ParentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ParentViewController.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+import UIKit
+
+class ParentViewController: UIViewController {
+    
+    private let navigationView = NavigationView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(navigationView)
+    }
+}
+
