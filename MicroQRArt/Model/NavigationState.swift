972a95ec26ade7f29a7241511244a27db60419df
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-29 16:13:08 +0900

    feat(state): タブの状態を管理するプロパティを追加

--- MicroQRArt/Model/NavigationState.swift
+++ MicroQRArt/Model/NavigationState.swift
@@ -17,4 +17,19 @@ struct NavigationState {
     func updated(currentIndex: Int) -> NavigationState {
         return NavigationState(currentIndex: currentIndex, availableTabs: availableTabs)
     }
+
+    var buttonStates: [NavigationButtonState] {
+        return availableTabs.map { type in
+            let isSelected = (type.index == currentIndex)
+            return NavigationButtonState(
+                type: type,
+                iconName: type.iconName,
+                isSelected: isSelected
+            )
+        }
+    }
+    
+    var currentTabType: RootViewControllerType? {
+        return availableTabs.first { $0.index == currentIndex }
+    }
 }
