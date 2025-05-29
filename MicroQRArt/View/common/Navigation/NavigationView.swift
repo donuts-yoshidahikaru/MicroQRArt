5a20c2349d53d57bead8203628628a9620fddb21
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-29 16:16:51 +0900

    fix(view): NavigationViewのレイアウトを修正

--- MicroQRArt/View/common/Navigation/NavigationView.swift
+++ MicroQRArt/View/common/Navigation/NavigationView.swift
@@ -22,11 +22,12 @@ final class NavigationView: UIView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        contentView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: fixedHeight)
-    }
+        let width = UIScreen.main.bounds.width
+        let height: CGFloat = fixedHeight + self.safeAreaInsets.bottom
+        
+        contentView.frame = CGRect(x: 0, y: 0, width: width, height: fixedHeight)
+        self.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - height, width: width, height: height)
 
-    override var intrinsicContentSize: CGSize {
-        CGSize(width: UIView.noIntrinsicMetric, height: fixedHeight + safeAreaInsets.bottom)
     }
     
     required init?(coder: NSCoder){ fatalError("init(coder:) has not been implemented"); }
