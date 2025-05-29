bd065a4e45b602c50c42cfcf28b7db9abae632bb
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-29 11:26:20 +0900

    feat(enum): ParentVCTypeの名前を変更
    - ParentViewControllerType -> RootViewControllerType

---
+++ MicroQRArt/Value/RootViewControllerType.swift
@@ -0,0 +1,12 @@
+//
+//  RootViewControllerType.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/05/28.
+//
+
+enum RootViewControllerType {
+    case decoration
+    case home
+    case profile
+} 
