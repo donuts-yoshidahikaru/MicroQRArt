ea13fad3117bc8bd545fe82f2598bf3e9f0acd30
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-29 16:36:15 +0900

    feat(view): キーボード非表示機能を追加

--- MicroQRArt/ViewController/Home/HomeViewController.swift
+++ MicroQRArt/ViewController/Home/HomeViewController.swift
@@ -8,7 +8,30 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
+    @IBOutlet weak var sourceTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupKeyboardDismissRecognizer()
+        sourceTextField.returnKeyType = .done
+        sourceTextField.delegate = self
+    }
+
+    private func setupKeyboardDismissRecognizer() {
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapRecognizer)
+    }
+
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension HomeViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
