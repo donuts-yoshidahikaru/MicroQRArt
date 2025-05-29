735e4888ec872b5535e463316c66960310e9da5b
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-29 11:26:54 +0900

    feat(enum): RootViewControllerTypeにインデックスプロパティを追加

--- MicroQRArt/Value/RootViewControllerType.swift
+++ MicroQRArt/Value/RootViewControllerType.swift
@@ -9,4 +9,14 @@ enum RootViewControllerType {
     case decoration
     case home
     case profile
+
+    var index: Int? {
+        get {
+            switch self {
+            case .decoration: return 0
+            case .home: return 1
+            case .profile: return 2
+            }
+        }
+    }
 } 
