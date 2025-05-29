023bba167ec687172727e6c87691283cb7414840
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-05-29 16:15:25 +0900

    feat(view): NavigationContentViewにボタンの状態を設定する機能を追加

--- MicroQRArt/View/common/Navigation/NavigationContentView.swift
+++ MicroQRArt/View/common/Navigation/NavigationContentView.swift
@@ -6,25 +6,60 @@
 //
 
 import UIKit
+import Combine
 
 protocol NavigationContentViewDelegate: AnyObject {
     func navigationContentView(_ view: NavigationContentView, didTap type: RootViewControllerType)
 }
 
 final class NavigationContentView: UIView {
-    weak var delegate: NavigationContentViewDelegate?
 
+    // MARK: - IBOutlets
+    @IBOutlet private weak var decorationButton: UIButton!
+    @IBOutlet private weak var homeButton: UIButton!
+    @IBOutlet private weak var profileButton: UIButton!
+
+    // MARK: - Properties
+    weak var delegate: NavigationContentViewDelegate?
+    private var cancellables = Set<AnyCancellable>()
+
+    // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
         let view = UINib(nibName: "NavigationContentView", bundle: nil).instantiate(withOwner: self, options: nil).first as! UIView
         view.frame = bounds
         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         addSubview(view)
     }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    func configure(with buttonStates: [NavigationButtonState]) {
+        for buttonState in buttonStates {
+            updateButton(for: buttonState)
+        }
+    }
+    
+    private func updateButton(for state: NavigationButtonState) {
+        let button = getButton(for: state.type)
+        let systemImage = UIImage(systemName: state.systemIconName)
+        
+        button?.setImage(systemImage, for: .normal)
+    }
+    
+    private func getButton(for type: RootViewControllerType) -> UIButton? {
+        switch type {
+        case .decoration: return decorationButton
+        case .home: return homeButton
+        case .profile: return profileButton
+        }
     }
 }
 
