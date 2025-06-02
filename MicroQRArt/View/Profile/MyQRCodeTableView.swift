a1cdb801fd9c34ea31ce7106675f337a403c0a01
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-06-02 12:58:49 +0900

    feat(view): MyQRCodeTableViewにRxSwiftを導入し、データバインディング機能を追加

--- MicroQRArt/View/Profile/MyQRCodeTableView.swift
+++ MicroQRArt/View/Profile/MyQRCodeTableView.swift
@@ -6,48 +6,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class MyQRCodeTableView: UITableView {
 
     // MARK: - Properties
     private let cellHeight: CGFloat = 80
+    private let disposeBag = DisposeBag()
 
-    // MARK: - Sample Data
-    private let qrCodes = [
-        (title: "自宅Wi-Fi", source: "https://wifi.example.com/qr/1234567890", date: "2025/05/30"),
-        (title: "会社Wi-Fi", source: "https://wifi.example.com/qr/0987654321", date: "2025/05/29"),
-        (title: "名刺QR", source: "https://meishi.example.com/qr/1111111111", date: "2025/05/28"),
-        (title: "イベント入場QR", source: "https://event.example.com/qr/2222222222", date: "2025/05/27")
-    ]
-    
     // MARK: - Initialization
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
         backgroundColor = UIColor.background
+        rowHeight = cellHeight
         separatorStyle = .none
-        dataSource = self
-        delegate = self
         register(MyQRCodeTableViewCell.self, forCellReuseIdentifier: "MyQRCodeTableViewCell")
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
-
-// MARK: - UITableViewDelegate, UITableViewDataSource
-extension MyQRCodeTableView: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return qrCodes.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyQRCodeTableViewCell", for: indexPath) as? MyQRCodeTableViewCell else {
-            return UITableViewCell()
-        }
-        let qr = qrCodes[indexPath.row]
-        cell.configure(title: qr.title, source: qr.source, date: qr.date)
-        return cell
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return cellHeight
+    // MARK: - Public Methods
+    func bind<Source: ObservableType>(
+        source: Source,
+        dataSource: RxTableViewSectionedReloadDataSource<QRCodeSection>
+    ) where Source.Element == [QRCodeSection] {
+        source
+            .bind(to: self.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 }
