6d0183aea645e766923868aa2c1903b7601825d2
donuts-yoshidahikaru <yoshida.hikaru@donuts.ne.jp>
2025-06-02 12:57:07 +0900

    feat(model): QRCodeItem構造体を新規作成

---
+++ MicroQRArt/Model/QRCodeItem.swift
@@ -0,0 +1,16 @@
+//
+//  QRCodeItem.swift
+//  MicroQRArt
+//
+//  Created by yoshida.hikaru on 2025/06/02.
+//
+
+import UIKit
+
+struct QRCodeItem: Identifiable, Equatable {
+    let id: String
+    let image: UIImage?
+    let title: String
+    let source: String
+    let date: String
+} 
